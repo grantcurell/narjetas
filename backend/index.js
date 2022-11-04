f42f5eba3db7e70ce4ab59bd4206495af8d965fd
Grant Curell <grant_curell@dell.com>
2022-11-03 20:53:10 -0400

    Upload

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,43 @@
-const {By,Key,Builder} = require("selenium-webdriver");
 const express = require('express');
-require("chromedriver");
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+const screen = {
+    width: 640,
+    height: 480
+};
 
 async function example(){
 
+    /*
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument("--test-type")
+        options.add_argument("--disable-web-security")
+
+        if headless:
+            options.add_argument("--headless")
+
+        if binary_location is not None:
+            options.binary_location = binary_location
+
+        driver = webdriver.Chrome(options=options)
+     */
+
     const searchString = "Automation testing with Selenium";
 
+    //let builder = new Builder().forBrowser('chrome')
+    //builder = builder.setChromeOptions(new chrome.Options().headless())
+
+    let driver = new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chrome.Options().headless().windowSize(screen))
+        .setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
+        .build();
+
     //To wait for browser to build and launch properly
-    let driver = await new Builder().forBrowser("chrome").build();
+    //let driver = await builder.forBrowser("chrome").build();
 
     //To fetch http://google.com from the browser with our code.
     await driver.get("http://google.com");
@@ -24,7 +54,7 @@ async function example(){
 
 }
 
-//example()
+example()
 const app = express();
 
 app.get('/', function (req, res) {

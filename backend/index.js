daee34f82dd2cd41b4d78a14967d7b6423880e68
Grant Curell <grant_curell@dell.com>
2022-11-22 09:07:31 -0500

    Try to save selenium from killing itself

--- backend/index.js
+++ backend/index.js
@@ -117,7 +117,11 @@ app.get('/geturl/:url', function(req, res) {
                 console.error("ERROR: Failed to quit driver.");
             })
         }, 1000);
-    });
+    }).catch((error) => {
+        console.error(`A critical error was thrown while processing 
+        ${req.params.url}. Error was ${error}`);
+        res.send('');
+    });;
 
 })
 
@@ -140,7 +144,11 @@ app.get('/geturlwithclass/:url/:class', function(req, res) {
                 console.error("ERROR: Failed to quit driver.");
             })
         }, 1000);
-    });
+    }).catch((error) => {
+        console.error(`A critical error was thrown while processing 
+        ${req.params.url}. Error was ${error}`);
+        res.send('');
+    });;
 
 })
 
@@ -164,7 +172,11 @@ app.get('/geturlwithid/:url/:id', function(req, res) {
                 console.error("ERROR: Failed to quit driver.");
             })
         }, 1000);
-    });
+    }).catch((error) => {
+        console.error(`A critical error was thrown while processing 
+        ${req.params.url}. Error was ${error}`);
+        res.send('');
+    });;
 
 })
 
@@ -188,6 +200,10 @@ app.get('/getordbokene/:searchWord/:type', function(req, res) {
                     console.error("ERROR: Failed to quit driver.");
                 })
             }, 1000);
+        }).catch((error) => {
+            console.error(`A critical error was thrown while processing 
+            ${req.params.searchWord}. Error was ${error}`);
+            res.send('');
         });
     } else if (req.params.type === "etymology") {
         Ordbokene.getOrdbokeneEtymology(driver, req.params.searchWord).then(html => {
@@ -199,7 +215,11 @@ app.get('/getordbokene/:searchWord/:type', function(req, res) {
                     console.error("ERROR: Failed to quit driver.");
                 }
             }, 1000);
-        });
+        }).catch((error) => {
+            console.error(`A critical error was thrown while processing 
+            ${req.params.searchWord}. Error was ${error}`);
+            res.send('');
+        });;
     } else {
         Ordbokene.getOrdbokeneDefs(driver, req.params.searchWord).then(html => {
             res.send(html);
@@ -210,7 +230,11 @@ app.get('/getordbokene/:searchWord/:type', function(req, res) {
                     console.error("ERROR: Failed to quit driver.");
                 })
             }, 1000);
-        });
+        }).catch((error) => {
+            console.error(`A critical error was thrown while processing 
+            ${req.params.searchWord}. Error was ${error}`);
+            res.send('');
+        });;
     }
     
 
